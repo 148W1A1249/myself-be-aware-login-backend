562832e153a42170e76d830b2c1eb30a30ed564b
khasim sharif shaik <khasimsharif30@gmail.com>
2021-06-05 21:42:14 +0530

    remove unwanted data

--- index.js
+++ index.js
@@ -19,10 +19,3 @@ connectDB
 })
 .catch(error => console.log(error))
 
-// https://www.youtube.com/watch?v=Va9UKGs1bwI
-// https://www.youtube.com/watch?v=kfw61IxDgW8
-// https://www.youtube.com/watch?v=CEim3tZsp1Y
-// https://www.freecodecamp.org/news/introduction-to-mongoose-for-mongodb-d2a7aa593c57/#:~:text=Mongoose%20is%20an%20Object%20Data,js.&text=MongoDB%20is%20a%20schema%2Dless,not%20enforced%20like%20SQL%20databases.
-
-
-
