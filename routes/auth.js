c3b8c5be37489e6d2449488f848db53c5772366f
khasim sharif shaik <khasimsharif30@gmail.com>
2021-06-05 21:33:06 +0530

    add routes

--- routes/auth.js
+++ routes/auth.js
@@ -7,8 +7,8 @@ const {signup, activateAccount} = require("../controllers/auth_signup");
 const {forgotPassword,resetPassword} = require("../controllers/auth_forgot");
 
 // signup page routes
-// router.get('/email-authentication', emailAuthentication);
-// router.get('/user-details', userDetails);
+router.get('/email-authentication', emailAuthentication);
+router.get('/user-details', userDetails);
 
 // signup page routes
 router.post('/signup', signup);
